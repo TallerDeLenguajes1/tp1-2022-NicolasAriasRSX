ccf05b357277e25e004d874b9ed08a615df39a05
NicolasArias <ariasmec06@gmail.com>
2022-08-29 11:23:45 -0300

    problema 3 listo

---
+++ problema3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+Console.WriteLine("Rendimiento Kilomentros por Litros\n");
+
+try
+{
+    int consumo;
+    consumo= Rendimento();
+    Console.WriteLine("Rendimiento= " + consumo + " km/lts");
+}
+catch (System.FormatException)
+{
+    Console.WriteLine("\n- - - - - - - - - -\nError: Formato incorrecto.\n");
+}
+catch (System.DivideByZeroException)
+{
+    Console.WriteLine("\n- - - - - - - - - -\nError: No esta definida la divicion por 0 (cero).\n");
+}
+catch (System.OverflowException)
+{
+    Console.WriteLine("\n- - - - - - - - - -\nError: El valor era demasiado grande o demasiado pequeño para un Int32..\n");
+}
+catch (System.Exception ex)
+{
+    Console.WriteLine("\n- - - - - - - - - -\nError: " + ex.Message + "\n");
+}
+
+
+
+
+//-------------->Funcion<----------------------
+int Rendimento()
+{
+    int kilomentros, litros;
+    Console.WriteLine("Ingrese la cantidad de Kilometros realizados: ");
+    kilomentros= Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ingrese los litros consumidos: ");
+    litros= Convert.ToInt32(Console.ReadLine());
+    return kilomentros / litros;
+}
