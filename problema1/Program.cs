946ccf5523c719b49a8514c1f9af5603b20c8e7a
NicolasArias <ariasmec06@gmail.com>
2022-08-29 10:17:45 -0300

    problema 1 y 2 listos

---
+++ problema1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+int numero;
+Console.WriteLine("Ingrese un numero para calcular su cuadrado: ");
+try
+{
+    numero= Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Su cuadrado es: " + Cuadrado(numero));
+}
+catch (System.Exception ex)
+{
+    Console.WriteLine("\nError: " + ex.Message + "\n");
+}
+
+int Cuadrado(int num)
+{
+    return num * num;
+}
---
+++ problema2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+Console.WriteLine("Dividir.");
+
+int divisor, dividendo, resultado;
+
+try
+{
+    Console.WriteLine("Ingrese el dividendo: ");
+    dividendo= Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ingrese el divisor: ");
+    divisor= Convert.ToInt32(Console.ReadLine());
+    resultado= dividendo / divisor;
+
+    Console.WriteLine("El resultado es: " + resultado);    
+}
+catch (System.FormatException)
+{
+    Console.WriteLine("\nError: Formato incorrecoto.\n");
+}
+catch (System.DivideByZeroException)
+{
+    Console.WriteLine("\nError: No esta definida la divicion por 0 (cero).\n");
+}
+catch (System.OverflowException)
+{
+    Console.WriteLine("\nError: Numero demasiado grande o demasiado pequeño.\n");
+}
+catch (System.Exception ex)
+{
+    Console.WriteLine("\n" + ex.Message + "\n");
+}
